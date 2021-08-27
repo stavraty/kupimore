a1b563646091bbce78a2d6275fc6d4aa38a0284c
Andrey <privatecapacity.dev@icloud.com>
2021-08-27 21:46:31 +0300

    Initial Commit

---
+++ myskill/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  myskill
+//
+//  Created by Private Capacity Ltd on 04.08.2020.
+//  Copyright © 2020 PRIVATE CAPACITY LTD. All rights reserved.
+//
+
+//
+//  ViewController.swift
+//  myskill
+//
+//  Created by Private Capacity Ltd on 04.08.2020.
+//  Copyright © 2020 PRIVATE CAPACITY LTD. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    //MARK:- Variable Declarations
+
+    @IBOutlet var mWKWebView: WKWebView!
+    
+    //MARK:- ViewController LifeCycle Methods
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        mWKWebView = WKWebView(frame: self.view.frame)
+        mWKWebView.uiDelegate = self
+        mWKWebView.navigationDelegate = self
+        view.addSubview(mWKWebView)
+
+        let url = URL(string: "https://kupimore.ru")
+        let request = URLRequest(url: url!)
+
+        mWKWebView.load(request)
+    
+
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    func webView(_ WebView: WKWebView,
+                 runJavaScriptAlertPanelWithMessage message: String,
+                 initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping () -> Void) {
+
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let title = NSLocalizedString("OK", comment: "OK Button")
+        let ok = UIAlertAction(title: title, style: .default) { (action: UIAlertAction) -> Void in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(ok)
+        present(alert, animated: true)
+        completionHandler()
+    }
+
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            completionHandler(true)
+        }))
+
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
+            completionHandler(false)
+        }))
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+}
+

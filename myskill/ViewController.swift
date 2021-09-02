713238d96ec118a745273dad4851bf8218ab6324
Andrey <privatecapacity.dev@icloud.com>
2021-09-02 10:53:16 +0300

    Reachability

--- myskill/ViewController.swift
+++ myskill/ViewController.swift
@@ -16,14 +16,12 @@
 
 import UIKit
 import WebKit
-import SwiftUI
+
+let reachability = try! Reachability()
 
 class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
     //MARK:- Variable Declarations
-    
-    @ObservedObject var monitor = NetworkMonitor()
-    @State private var showAlertSheet = false
 
     @IBOutlet var mWKWebView: WKWebView!
     
@@ -32,7 +30,7 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     override func viewDidLoad() {
 
         super.viewDidLoad()
-
+        
         mWKWebView = WKWebView(frame: self.view.frame)
         mWKWebView.uiDelegate = self
         mWKWebView.navigationDelegate = self
@@ -42,56 +40,38 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
         let request = URLRequest(url: url!)
 
         mWKWebView.load(request)
+        
+      // R
+        
+        reachability.whenReachable = { reachability in
+            if reachability.connection == .wifi {
+                print("Reachable via wifi")
+            }else{
+                print("Reachable via cellular")
+            }
+        }
+        
+        reachability.whenUnreachable = { _ in
+            print("Not reachable")
+            self.showAlert()
+        }
+        
+        do {
+        try reachability.startNotifier()
+        }catch{
+            print("unable to start notifier")
+        }
+    }
     
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "No internet", message: "This App Requires wifi/internet connection!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default Action"),
+                                      style: .default, handler: {_ in
+                                        NSLog("The \"OK \" alert occured.")
+                                      }))
+        self.present(alert, animated: true, completion: nil)
     }
+}
 
-    func webView(_ WebView: WKWebView,
-                 runJavaScriptAlertPanelWithMessage message: String,
-                 initiatedByFrame frame: WKFrameInfo,
-                 completionHandler: @escaping () -> Void) {
-
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let title = NSLocalizedString("OK", comment: "OK Button")
-        let ok = UIAlertAction(title: title, style: .default) { (action: UIAlertAction) -> Void in
-            alert.dismiss(animated: true, completion: nil)
-        }
-        alert.addAction(ok)
-        present(alert, animated: true)
-        completionHandler()
-    }
-
-    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
-
-        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
-
-        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
-            completionHandler(true)
-        }))
-
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
-            completionHandler(false)
-        }))
-
-        self.present(alertController, animated: true, completion: nil)
-    }
-// NetworkMonitor
-    var body: some View {
-        VStack {
-            Image(systemName: monitor.isConnected ? "wifi" : "wifi.slash")
-                .font(.system(size: 56))
-            Text(monitor.isConnected ? "Connected!" : "Not connected")
-                .padding()
-            
-            Button("Perfom Network Request") {
-                self.showAlertSheet = true
-            }
-        }.alert(isPresented: $showAlertSheet, content: {
-            if monitor.isConnected {
-                return Alert(title: Text("Success!"), message: Text("The network request can be performed."), dismissButton: .default(Text("OK")))
-            }
-            return Alert(title: Text("No Internet Connection"), message: Text("Please enable WiFi or Cellular data"), dismissButton: .default(Text("Cansel")))
-        })
-        }
-    }
-// NetworkMonitor
 
